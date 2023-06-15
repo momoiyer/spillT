143b6229264e60bc5c4b7b81f78490b44353c560
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-06-15 12:23:09 -0600

    Create textbox

--- front-end/components/CreatePost.tsx
+++ front-end/components/CreatePost.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div>
+      <h3>Create post</h3>
+      <form action="" method="post">
+        <label>What's on your mind?</label>
+        <input type="text" id="postText" name="postText" />
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
