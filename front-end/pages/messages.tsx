058a2cc21dd4e6a5bc1eeb479f837d5137c12f23
MoMoZin <khinmomozin@gmail.com>
2023-07-04 16:55:03 -0600

    update color theme

--- front-end/pages/messages.tsx
+++ front-end/pages/messages.tsx
@@ -7,7 +7,7 @@ const Messages = () => {
   return (
     <section className="flex justify-around p-5 bg-cyan-100 dark:bg-cyan-800 dark:text-white">
       {/* Left side section with search bar and chat preview */}
-      <article className="basis-1/4 bg-cyan-200 dark:bg-cyan-700">
+      <article className="basis-1/4 bg-white dark:bg-cyan-700">
         <ChatFriendSearch styleClass="p-5" />
         <ChatPreviewList styleClass="p-5" />
       </article>
@@ -22,7 +22,7 @@ const Messages = () => {
           <p className="pl-5">Jordan King</p>
         </div>
         <ChatConversation styleClass="p-5 overflow-y-auto h-[65vh]" />
-        <input type="text" id="first_name" className="bg-cyan-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter message here" />
+        <input type="text" id="first_name" className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter message here" />
 
       </article>
     </section>
