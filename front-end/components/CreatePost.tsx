a27d1210dc5990011d2bd1bfc585a8094ea998ac
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-07-19 15:23:19 -0600

    Add margin and positions

--- front-end/components/CreatePost.tsx
+++ front-end/components/CreatePost.tsx
@@ -1,6 +1,22 @@
+import ProfilePicture from "./common/ProfilePicture";
+
 const CreatePost = () => {
   return (
-    <div className="flex-col w-[32rem]">
+    <div className="flex flex-col justify-items-center w-[32rem] shadow-lg p-3 rounded-lg">
+      <div className="flex justify-center relative">
+        <span className="font-semibold">Create post</span>
+        <p className="absolute top-0 right-0">x</p>
+      </div>
+      <div className="flex items-center mt-2 mb-2">
+        <ProfilePicture
+          imageUrl="/profile.png"
+          altText="Profile Picture"
+          imageWidth={50}
+          imageHeight={30}
+          styleClass="p-2"
+        />
+        <p>Sally Jenkins</p>
+      </div>
       <div>
         <input
           type="text"
@@ -10,7 +26,7 @@ const CreatePost = () => {
         />
         <button
           type="submit"
-          className="text-white absolute bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-[32rem]"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full mt-3"
         >
           Post
         </button>
